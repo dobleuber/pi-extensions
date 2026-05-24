5a460e9c7e1c0fbed61c78358f7674188878d32a
dobleuber <dobleuber@gmail.com>
2026-05-24 15:42:20 -0500

    Revert "Add Pi router extension scaffold"
    This reverts commit 584c6e6bb1f4a319a617b04def15462d597134f7.

--- extensions/pi-router/index.ts
+++
@@ -1,7 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-
-export default function piRouterExtension(pi: ExtensionAPI) {
-	pi.on("input", async (_event) => {
-		return { action: "continue" };
-	});
-}
